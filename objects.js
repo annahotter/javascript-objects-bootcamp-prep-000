43eb660db7c58daa750a135464de62d246afc25b
Anna Hotter <annajhotter@gmail.com>
2017-12-09 22:41:48 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,1 @@
 var playlist = { [mountainGoats]: 'color' }
-
-function updateObjectWithKeyAndValue ({obj, key, value}
-  Object.assign({}, obj, {[key]:value})
-)
-const playlist = {toveLo: 'Habits'}
