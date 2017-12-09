050bb43dd1c158fc12118af137b14e839a546af8
Anna Hotter <annajhotter@gmail.com>
2017-12-09 22:50:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 var playlist = { kanye: "stronger"};
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, obj, { [key]: value})
+  return Object.assign({}, playlist, { [key]: value})
 }
