084895c107197316180cfcaa7c048a0e4adf1954
Anna Hotter <annajhotter@gmail.com>
2017-12-09 22:54:11 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, { ['Phil Ochs']: "Here's to the State of Mississippi"})
 }
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.kanye;
+  delete playlist.Kanye;
   return playlist
 }
