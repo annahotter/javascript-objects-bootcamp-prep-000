fc667430853debaef1accfc76cc9b632ea38f813
Anna Hotter <annajhotter@gmail.com>
2017-12-09 22:36:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,2 +1,7 @@
 var playlist = {theMountainGoats: "Color in your Cheeks",
 sujanStevens: "Silver and Gold"};
+
+function updateObjectWithKeyAndValue ({obj, key, value}
+  Object.assign({}, obj, {[key]:value})
+)
+  const playlist = {: ''}
